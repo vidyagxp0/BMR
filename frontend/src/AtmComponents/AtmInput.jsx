e706293a00462380129150ff3f17ee0664d60f69
sandhyaprajapati7354 <sandhya.prajapati@vidyagxp.com>
2024-08-28 10:01:15 +0530

    css changes

--- frontend/src/AtmComponents/AtmInput.jsx
+++ frontend/src/AtmComponents/AtmInput.jsx
@@ -11,7 +11,7 @@ const AtmInput = ({ name, type = 'text', placeholder = '', value, onChange, clas
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className={`px-4 py-2  border rounded-md text-white focus:outline-none focus:ring-2 focus:ring-blue-300 transition ease-out duration-300 ${className}`}
+        className={`px-4 py-2  border rounded-md text-black focus:outline-none focus:ring-2 focus:ring-blue-300 transition ease-out duration-300 ${className}`}
         
       />
        {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
